cf3ccadef389f2efda36c2ce16963b037a93adfa
Ferdinand Niemann <hello@ferdi.digital>
2024-10-17 17:35:04 +0200

    implement fetch for all reicpes

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,19 @@
   <h1>Rezept-App</h1>
   <RouterView />
 </template>
+
+<script>
+import { useRecipesStore } from '@/stores/recipes.js'
+
+export default {
+  data() {
+    return {
+      state: useRecipesStore(),
+    }
+  },
+
+  created() {
+    this.state.getAllRecipes()
+  },
+}
+</script>
