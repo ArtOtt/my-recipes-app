e1b95efcffaf29b2a35ff892e9685b1f206e9a76
Arthur Ott <arthur.ott91@web.de>
2024-11-01 12:20:48 +0100

    complet task 4

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -14,16 +14,19 @@ export default {
 <style scoped>
 header {
   max-width: fit-content;
-  padding-block: 1rem;
+  padding-block: 0.5rem;
   border-bottom: 1px solid midnightblue;
 }
 h2 {
   color: midnightblue;
   font-weight: 400;
+
+  padding-top: 1rem;
 }
 
 p {
   font-size: medium;
-  color: darkgray;
+  color: rgb(41, 36, 36);
+  padding-top: 1rem;
 }
 </style>
