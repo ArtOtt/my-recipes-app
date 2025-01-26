330338a5038f41de50cd9a32aeb48434d5cbe834
Arthur Ott <arthur.ott91@web.de>
2025-01-26 11:52:54 +0100

    add tailwind and pinia

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import { useRecipesStore } from "./stores/recipes";
+
+const test = useRecipesStore().test;
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h2>Hallo Welt</h2>
+    <h2>{{ test }}</h2>
   </div>
 </template>
